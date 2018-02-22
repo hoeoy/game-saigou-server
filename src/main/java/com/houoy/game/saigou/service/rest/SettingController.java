ad0d531a3008c578d7f9e11a594fedc110bf32ae
andyzhaozhao <andy_zhaozhao@hotmail.com>
2018-02-22 16:05:39 +0800

    Add:增加对redis支持。赔率调整。每期用户下注概览。每期总概览

--- src/main/java/com/houoy/game/saigou/service/rest/SettingController.java
+++ src/main/java/com/houoy/game/saigou/service/rest/SettingController.java
@@ -1,14 +1,15 @@
 package com.houoy.game.saigou.service.rest;
 
 
+import com.houoy.common.vo.RequestResultVO;
 import com.houoy.game.saigou.service.CacheService;
 import com.houoy.game.saigou.vo.RateVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by andyzhao on 2017-02-20.
@@ -29,6 +30,46 @@ public class SettingController {
         rateVO.setRateNum(Double.parseDouble(cacheService.get(RateVO.rateNumName)));
         rateVO.setRateTwo(Double.parseDouble(cacheService.get(RateVO.rateTwoName)));
         return rateVO;
+    }
+
+    @ApiOperation(value = "设置赔率参数")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "rateVO", value = "赔率信息", required = true, paramType = "body", dataType = "RateVO")
+    })
+    @PostMapping("/save")
+    public RequestResultVO save(@RequestBody RateVO rateVO) {
+        RequestResultVO resultVO = new RequestResultVO();
+        if (rateVO != null) {
+            Double odds = rateVO.getOdds();
+            Double rateNum = rateVO.getRateNum();
+            Double rateTwo = rateVO.getRateTwo();
+
+            if (odds != null && odds > 0 && odds <= 1) {
+                cacheService.set(RateVO.oddsName, odds + "");
+            } else {
+                resultVO.setSuccess(false);
+                resultVO.setMsg("odds应大于0小于1");
+            }
+
+            if (rateNum != null && rateNum > 0 && rateNum < 10) {
+                cacheService.set(RateVO.rateNumName, rateNum + "");
+            } else {
+                resultVO.setSuccess(false);
+                resultVO.setMsg("rateNum应大于0小于10");
+            }
+
+            if (rateTwo != null && rateTwo > 0 && rateTwo < 2) {
+                cacheService.set(RateVO.rateTwoName, rateTwo + "");
+            } else {
+                resultVO.setSuccess(false);
+                resultVO.setMsg("rateTwo应大于0小于2");
+            }
+        }
+        resultVO.setSuccess(true);
+        resultVO.setMsg("保存成功");
+        resultVO.setResultData(1);
+
+        return resultVO;
     }
 }
 
